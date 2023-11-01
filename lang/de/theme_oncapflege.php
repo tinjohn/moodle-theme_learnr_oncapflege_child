56039fea306870c9348cc8a6186c7642138a5db0
tinjohn <tina.john@th-luebeck.de>
2023-11-01 21:13:36 +0100

    override: dynamic instruction text length in login form

---
+++ lang/de/theme_oncapflege.php
@@ -0,0 +1,6 @@
+<?php
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();   
+
