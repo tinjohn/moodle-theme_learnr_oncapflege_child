9b99f5915ec097d7eb1eae5406c97c4f892a5568
tinjohn <tina.john@th-luebeck.de>
2023-11-06 22:33:44 +0100

    more universal

--- amd/src/signupwizard.js
+++ amd/src/signupwizard.js
@@ -117,7 +117,13 @@ export const init = () => {
     for(var x=1; x<tabPanels.length; x++) {
         window.console.log(tabPanels[x]);
     }
-    const steps = [5, 9]; // Initialize the array with 6 and 10
+    var steps = [];
+    if(fieldsets.length > 1) {
+        steps = [6, 10]; // Initialize the array with 6 and 10
+    } else {
+        steps = [5, 9]; // Initialize the array with 6 and 10
+    }
+
     for (let x = 1; x < fieldsets.length; x++) {
         steps.push(10 + x); // Add 10 + x to the array for each x from 1 to the length of fieldsets
     }
