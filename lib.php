1340c10b84974a44cd4af2193d7a93d61b5abe2b
tinjohn <tina.john@th-luebeck.de>
2023-11-06 11:57:54 +0100

    pre and raw scss moved in hard coded scss
    maybe not the last solution

--- lib.php
+++ lib.php
@@ -10,8 +10,9 @@ defined('MOODLE_INTERNAL') || die();
 function theme_oncapflege_get_main_scss_content($theme) {                                                                                
     global $CFG;                                                                                                                    
                                                                                                                                     
-    $scss = '';                                                                                                                     
-    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;                                                 
+    $scss = '';     
+    $themeparent = theme_config::load('learnr');                                                                                                                
+    $filename = !empty($themeparent->settings->preset) ? $themeparent->settings->preset : null;                                                 
     $fs = get_file_storage();                                                                                                       
                                                                                                                                     
     $context = context_system::instance();                                                                                          
@@ -21,8 +22,8 @@ function theme_oncapflege_get_main_scss_content($theme) {
     } else if ($filename == 'plain.scss') {                                                                                         
         // We still load the default preset files directly from the boost theme. No sense in duplicating them.                      
         $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');                                          
-                                                                                                                                    
-    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_oncapflege', 'preset', 0, '/', $filename))) {              
+    // theme_oncapflege is very important here maybe not                                                                                                                                
+    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_learnr', 'preset', 0, '/', $filename))) {              
         // This preset file was fetched from the file area for theme_oncapflege and not theme_boost (see the line above).                
         $scss .= $presetfile->get_content();                                                                                        
     } else {                                                                                                                        
