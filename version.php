bc277427b3ca51341ea8b903cf9a9eaf892fcf41
tinjohn <tina.john@th-luebeck.de>
2023-11-13 20:56:57 +0100

    placeholder added

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') || die();                                                                                                
                                                                                                                                     
 // This is the version of the plugin.                                                                                               
-$plugin->version = '2023111300';                                                                                                    
+$plugin->version = '2023111301';                                                                                                    
                                                                                                                                     
 // This is the version of Moodle this plugin requires.                                                                              
 $plugin->requires = '2022041908';                                                                                                   
